8665b599e2f7dbc1130dfa6f629a7df606a3ddc8
ruanfelixdev <ruanfeliz7777@gmail.com>
2026-05-17 16:04:53 -0300

    Delete versão 2  directory

--- versão 2 /modulos.py
+++
@@ -1,9 +0,0 @@
-def continuar_ou_nao():
-    while True:
-        opc = input('deseja continuar? (s pra sim n pra nao)')
-        if opc == 'n':
-            return False
-        elif opc == 's':
-            return True
-        else:
-            print('opção invalida')
